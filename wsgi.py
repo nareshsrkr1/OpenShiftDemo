52c0a93792a47ba05011d81ac9d9bf2e1295474f
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2025-02-07 11:00:27 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
-application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    application.run()
+@app.route('/')
+def home():
+    return "Hello, OpenShift! This is a Python Flask app."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
